9fb16bdd5194665b2be7b33bfc33db22410c9544
Salik Ashraf <75222981+Salik147@users.noreply.github.com>
2023-09-14 10:19:58 +0500

    Add files via upload
    adding temorary files

---
+++ WebForm2.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed2_Click(object sender, EventArgs e)
+        {
+            string enteredId = TextBox1.Text.Trim();
+            
+
+            if (enteredId == "user" )
+            {
+                // Redirect to admin.aspx
+                Response.Redirect("UserPanel.aspx");
+            }
+            else
+            {
+                // Display "wrong password" message
+                Label3.Text = "Wrong information";
+            }
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
